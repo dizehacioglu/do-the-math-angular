f78818f77be64d196f022624121cdb337ef35c0f
Dize Hacioglu <dh1272@nyu.edu>
2015-09-13 19:13:30 -0600

    added server and implemented taxee api

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/', function(req, res) {
+	res.render('index');
+});
+
+var server = app.listen(7050, function() {
+	console.log('Express server listening on port ' + server.address().port);
+});
---
+++ public/main.js
@@ -0,0 +1,49 @@
+var doTheMath = angular.module('doTheMath', []);
+
+doTheMath.controller('doTheMathController', function($scope, $http){
+
+  // Static Vars
+  $scope.averageSalary = 67000;
+  $scope.averageTaxes = 17720;
+  $scope.averageNet = $scope.averageSalary -  $scope.averageTaxes; 
+
+  $scope.monthlyAmt = function(price){
+    return price / 12;
+  };
+
+  $scope.getTaxes = function(){
+
+    var payload = {
+      pay_rate: $scope.currentSalary,
+      filing_status: $scope.filingStatus,
+      pay_periods: $scope.payPeriods,
+      state: $scope.state,
+      year: '2014'
+    };
+
+    var req = {
+      method: 'POST',
+      url: 'http://taxee.io/api/v1/calculate/2014',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }, 
+      data: $.param(payload)
+    };
+
+    console.log(req);
+
+    
+    $http(req).success(function(data, status){
+
+      $scope.totalTaxes = data.annual.state.amount + data.annual.fica.amount + data.annual.federal.amount;
+
+      $scope.currentNet = $scope.currentSalary - $scope.totalTaxes;
+
+    })
+    .error(function(data, status){
+      console.log('error');
+    });
+
+  };
+
+});
